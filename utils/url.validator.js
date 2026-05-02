e4f6f6bb98c41a2d27d4fd20b50622e241ef5636
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-05-02 23:30:05 +0300

    fix: add comment

--- utils/url.validator.js
+++ utils/url.validator.js
@@ -2,7 +2,7 @@
 const { check } = require('express-validator');
 const {validatorMiddleware} = require('../middlewares/validator.middleware');
 
-
+// validate the long url
 exports.urlValidator = [
     check('longUrl').isURL().withMessage('Please enter a valid URL.'),
     validatorMiddleware
