57d18fb13aa0423f3a0bafb3196f24c14c48aedc
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-05-02 23:44:48 +0300

    chore: remove old backend service files

--- utils/url.validator.js
+++
@@ -1,9 +0,0 @@
-
-const { check } = require('express-validator');
-const {validatorMiddleware} = require('../middlewares/validator.middleware');
-
-// validate the long url
-exports.urlValidator = [
-    check('longUrl').isURL().withMessage('Please enter a valid URL.'),
-    validatorMiddleware
-];
